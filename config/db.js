1958ba0598cec58ec7bb25a5f2090535f0e56342
Dave Luhman <dave@ado.software>
2023-12-16 14:01:17 -0600

    create default user object

--- config/db.js
+++ config/db.js
@@ -1,4 +1,5 @@
 import { connect } from 'mongoose';
+import User from '../models/user.js'
 
 const connectDB = async () => {
     try {
@@ -10,3 +11,22 @@ const connectDB = async () => {
     }
 }
 export default connectDB
+
+async function isUsersCollectionEmpty() {
+    const user = await User.count()
+    return user === 0
+}
+
+async function createDefaultUser() {
+    try {
+        const user = await User.create({
+            password: '$2b$10$cDCSqQ17sAbWloBElfevMO9NmjORalQP/1VJ7WY6BwvB7PsuNM./m',
+            role: 'Admin',
+            email: 'admin@timekeeper.site',
+        })
+        return user
+    } catch (error) {
+        console.log(error)
+    }
+}
+if (await isUsersCollectionEmpty()) createDefaultUser().catch((err) => {console.log(err)})
