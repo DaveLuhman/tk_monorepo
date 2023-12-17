a7c505469b955193fece60a67b7125ce6f0ce797
Dave Luhman <dave@ado.software>
2023-12-17 13:44:14 -0600

    Sample JSON data for database hydration added

--- config/db.js
+++ config/db.js
@@ -1,12 +1,25 @@
 import { connect } from 'mongoose';
 import User from '../models/user.js'
-
+import TimeEntry from '../models/timeEntry.js';
+import { readFileSync } from 'fs'
+import { isUtf8 } from 'buffer';
 
 async function isUsersCollectionEmpty() {
     const user = (await User.find()).length
     return user === 0
 }
-
+async function isTimeEntryCollectionEmpty() {
+    const timeEntries = (await TimeEntry.find()).length
+    console.log(timeEntries)
+    return timeEntries === 0
+}
+async function importSampleTimeEntries() {
+    let sampleJson = JSON.parse(Buffer.from(readFileSync('./sampleEntries.json', {encoding: 'utf-8'})))
+    console.log(sampleJson)
+    sampleJson.map((entry) => {
+        TimeEntry.create(entry)
+    })
+}
 async function createDefaultUser() {
     try {
         const user = await User.create({
@@ -24,6 +37,7 @@ const connectDB = async () => {
         const conn = await connect(process.env.MONGO_URI);
         console.info(`MongoDB Connected: ${conn.connection.host}`.cyan.underline.bold)
         if (await isUsersCollectionEmpty()) createDefaultUser().catch((err) => {console.log(err)})
+        if (await isTimeEntryCollectionEmpty()) importSampleTimeEntries().catch((err) => {console.log(err)})
 
     } catch (err) {
         console.error(err.red);
