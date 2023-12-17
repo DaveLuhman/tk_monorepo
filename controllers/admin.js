79f192cccadfa6cd97ccf9269e20dc236a3a0d2d
Dave Luhman <dave@ado.software>
2023-12-17 13:11:17 -0600

    added dashboard path for admin role to see all TE

--- controllers/admin.js
+++ controllers/admin.js
@@ -7,11 +7,14 @@ const adminController = {}
 export default adminController
 
 adminController.getRoot = async (req, res) => {
+    let rawTimeEntries = []
     const { id } = req.user
     const user = await User.findById(id)
+    if(user.role === 'Admin'){ rawTimeEntries = await TimeEntry.find()}
+    else {
     const userCompany = await Customer.findById(user.company)
-    let timeEntries = await TimeEntry.find({sourceURL: {$eq: 'time.'+userCompany.rootDomain}})
-    const { trimmedData, page, pageCount  } = paginate(timeEntries, req.query.page || 1, 10)
+    rawTimeEntries = await TimeEntry.find({sourceURL: {$eq: 'time.'+userCompany.rootDomain}})}
+    const { trimmedData: timeEntries, page, pageCount  } = paginate(rawTimeEntries, req.query.page || 1, 10)
     res.locals.pagination = {page, pageCount}
     res.render('admin/dashboard', { timeEntries })
 }
