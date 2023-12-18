125b38bcc19e096511fa0a609b0702cb64324799
Dave Luhman <dave@ado.software>
2023-12-18 14:05:20 -0600

    started building groupedWeekly view stuck on separating [[yearly]] data into [[[wk][wk]...][year]] data

--- controllers/admin.js
+++ controllers/admin.js
@@ -13,10 +13,41 @@ adminController.getRoot = async (req, res) => {
     if (user.role === 'Admin') { rawTimeEntries = await TimeEntry.find() }
     else {
         const userCompany = await Customer.findById(user.company)
-        rawTimeEntries = await TimeEntry.find({ sourceURL: { $eq: 'time.' + userCompany.rootDomain } })
+        rawTimeEntries = await TimeEntry.find({ sourceURL: { $eq: 'time.' + userCompany.rootDomain } }).sort({dateSubmitted: -1})
     }
+    // switch(req.params.view){
+    //     case('weekly'): groupByWeek(rawTimeEntries)
+    //     case('name'): groupByName(rawTimeEntries)
+    // }
     const { trimmedData: timeEntries, targetPage:page, pageCount } = paginate(rawTimeEntries, req.query.p || 1, 10)
     res.locals.pagination = { page, pageCount }
     res.locals.timeEntries = timeEntries
     res.render('admin/dashboard')
+}
+
+function collectYearsFromEntries([entries]){
+    const yearsCollection = new Set()
+    entries.forEach((entry)=>{
+        yearsCollection.add(moment(entry.dateSubmitted).year())
+    })
+    return yearsCollection
+}
+
+function filterEntriesByYear(year,entry) {
+    return moment(entry.dateSubmitted).year() = year
+}
+
+function groupByWeek(entries){
+    const yearArray = []
+    const yearsList =  collectYearsFromEntries(entries)
+    yearsList.forEach(function(year) {
+        yearArray.push(entries.filter((moment(this.dateSubmitted)).year() === year))
+    })
+    const weekArray = [] // init array
+    for(i=0;i<52;i++){ // create 52 empty child arrays
+        weekArray.push([])
+    }
+    entries.forEach(function (entry){ //iterate through entries
+        weekArray[this.week].push(entry)  // add each entry to week array where index=weekOfYear
+    })
 }
