317f537e2b16d36e1d591aa309715dbabae76d3a
Dave Luhman <dave@ado.software>
2023-12-20 23:44:35 -0600

    built out skeleton for weekly-grouped view

--- controllers/admin.js
+++ controllers/admin.js
@@ -7,47 +7,54 @@ const adminController = {}
 export default adminController
 
 adminController.getRoot = async (req, res) => {
-    let rawTimeEntries = []
-    const { id } = req.user
-    const user = await User.findById(id)
-    if (user.role === 'Admin') { rawTimeEntries = await TimeEntry.find() }
-    else {
-        const userCompany = await Customer.findById(user.company)
-        rawTimeEntries = await TimeEntry.find({ sourceURL: { $eq: 'time.' + userCompany.rootDomain } }).sort({dateSubmitted: -1})
-    }
-    // switch(req.params.view){
-    //     case('weekly'): groupByWeek(rawTimeEntries)
-    //     case('name'): groupByName(rawTimeEntries)
-    // }
-    const { trimmedData: timeEntries, targetPage:page, pageCount } = paginate(rawTimeEntries, req.query.p || 1, 10)
+    const rawTimeEntries = await getUserScopedEntries(req.user)
+    const { trimmedData: timeEntries, targetPage: page, pageCount } = paginate(rawTimeEntries, req.query.p || 1, 10)
     res.locals.pagination = { page, pageCount }
     res.locals.timeEntries = timeEntries
     res.render('admin/dashboard')
 }
+adminController.getWeeklyGroupView = function (req, res) {
+    const entries = getUserScopedEntries(req.user)
+    const yearlySortedEntries = groupByYear(entries)
+    let weeklySortedEntries
+    yearlySortedEntries.forEach(function(year) {weeklySortedEntries.push(groupByWeek(year))})
+    res.send(weeklySortedEntries)
+}
 
-function collectYearsFromEntries([entries]){
+async function getUserScopedEntries(userId) {
+    const user = await User.findById(userId)
+    let rawTimeEntries = []
+    if (user.role === 'Admin') { rawTimeEntries = await TimeEntry.find()}
+    else {
+        const userCompany = await Customer.findById(user.company)
+        rawTimeEntries = await TimeEntry.find({ sourceURL: { $eq: 'time.' + userCompany.rootDomain } }).sort({ dateSubmitted: -1 })
+    }
+    return rawTimeEntries
+}
+
+function getYears([entries]) {
     const yearsCollection = new Set()
-    entries.forEach((entry)=>{
+    entries.forEach((entry) => {
         yearsCollection.add(moment(entry.dateSubmitted).year())
     })
     return yearsCollection
 }
-
-function filterEntriesByYear(year,entry) {
-    return moment(entry.dateSubmitted).year() = year
+function groupByYear(entries) {
+    const sortedArray = []
+    getYears(entries).forEach(year => {
+        sortedArray.push(entries.filter(function (entry) {
+            return moment(entry.dateSubmitted).year() === year
+        }))
+    })
+    return sortedArray
 }
-
-function groupByWeek(entries){
-    const yearArray = []
-    const yearsList =  collectYearsFromEntries(entries)
-    yearsList.forEach(function(year) {
-        yearArray.push(entries.filter((moment(this.dateSubmitted)).year() === year))
-    })
-    const weekArray = [] // init array
-    for(i=0;i<52;i++){ // create 52 empty child arrays
-        weekArray.push([])
+function groupByWeek(entries) {
+    const sortedArray = []
+    for (i = 0; i < 52; i++) { // create 52 empty child arrays
+        sortedArray.push([])
     }
-    entries.forEach(function (entry){ //iterate through entries
-        weekArray[this.week].push(entry)  // add each entry to week array where index=weekOfYear
+    entries.map(entry => {
+        sortedArray[entry.week].push(entry)
     })
+    return sortedArray
 }
