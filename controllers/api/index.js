b96c3180e36861a988d869c7c26a492770688e4c
Dave Luhman <dave@ado.software>
2024-01-08 01:39:56 -0600

    replaced named import with default import

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,6 @@
 import sanitizer from './sanitizer.js'
 import createMongoDocument from './mongo.js'
-import * as sendEmail from './sendgrid.js'
+import sendgridTemplateAPICall from './sendgrid.js'
 const controller = {}
 
 // route terminator that sanitizes input, creates the doc in the database, then sends the email and returns 202 Accepted
@@ -8,7 +8,7 @@ controller.submit = async (req, res) => {
     try {
         const doc = sanitizer(req.body)
         await createMongoDocument(doc)
-        sendEmail(doc)
+        sendgridTemplateAPICall(doc)
         res.status(202).send('Accepted');
     }
     catch (err) {
