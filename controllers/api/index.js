c6f5ea7cc60ab8836fdcb497437c7aa025585248
Dave Luhman <dave@ado.software>
2024-01-08 01:13:02 -0600

    updated sendgrid import for api submissiosn

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,6 @@
 import sanitizer from './sanitizer.js'
 import createMongoDocument from './mongo.js'
-
+import { sendgridTemplateAPICall as sendEmail } from './sendgrid.js'
 const controller = {}
 
 // route terminator that sanitizes input, creates the doc in the database, then sends the email and returns 202 Accepted
