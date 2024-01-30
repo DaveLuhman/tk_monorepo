49d04150a4aff02877b3ba51aa63e0e0bb4f2706
Dave Luhman <dave@ado.software>
2024-01-29 23:38:27 -0600

    Enhanced submission sanitization logic
    Refactored the sanitizeSubmission function to compute punch, hours, and
    overtime counts dynamically, ensuring data consistency. Transferring the
    responsibility of counting hours and overtime from client-side
    initialization in formDataToJSON to server-side processing in
    sanitizeSubmission addresses potential discrepancies and centralizes
    submission data formatting. Additionally, try-catch block integration
    enhances error handling during the sanitization process. Removed
    redundant client-side counters following the shift in logic to the
    backend.
    
    Resolves issue with mismatched submission data counts.

--- public/js/timeEntry.js
+++ public/js/timeEntry.js
@@ -143,9 +143,6 @@ function formDataToJSON() {
     empName,
     empEmail,
     timeEntries,
-    punchCount: activeEntries.length,
-    hoursCount: regHoursCounterEls[0].innerText,
-    overtimeCount: overtimeCounterEls[0].innerText,
     dateSubmitted: now,
     sourceURL: document.location.hostname,
   };
