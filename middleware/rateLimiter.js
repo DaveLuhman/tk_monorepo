c4795d24bdc17a3a685ba0735d14191f5989b326
Dave Luhman <dave@ado.software>
2024-05-26 17:24:56 -0500

    updated rate limiter to more than 10000

--- middleware/rateLimiter.js
+++ middleware/rateLimiter.js
@@ -47,7 +47,7 @@ async function checkEntriesCountAgainstPaymentTier(customer, hostname) {
             entriesLimit = 50
             break
         case 2:  // unlimited entries
-            entriesLimit = 100000
+            entriesLimit = 10000000
             //no limit, just here to catch this case
             break
     }
